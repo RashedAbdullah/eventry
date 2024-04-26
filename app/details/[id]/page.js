92706d0dabea8fff6bd30182b567e5cec9b09662
RashedAbdullah <maarashed40@gmail.com>
2024-04-26 12:49:43 +0600

    Booking project functionality

---
+++ app/details/[id]/page.js
@@ -0,0 +1,7 @@
+import Details from "@/components/details";
+
+const DetailsPage = () => {
+  return <Details />;
+};
+
+export default DetailsPage;
--- components/booking-section.jsx
+++ components/booking-section.jsx
@@ -1,8 +1,10 @@
 import BookingCard from "./booking-card";
+import Header from "./header";
 
 const BookingSection = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+      {/* <Header/> */}
       <BookingCard />
       <BookingCard />
       <BookingCard />
